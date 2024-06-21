7a548db1a2718e939e258700f6963ae77c99fd07
Eric Floyd <erixefb@gmail.com>
2024-06-21 13:42:22 -0400

    Basic Structure and Intalled Submodules

---
+++ todo/build/CMakeFiles/todo_lib.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/todo_lib.dir/main.cpp.o"
+  "CMakeFiles/todo_lib.dir/main.cpp.o.d"
+  "CMakeFiles/todo_lib.dir/todo.cpp.o"
+  "CMakeFiles/todo_lib.dir/todo.cpp.o.d"
+  "libtodo_lib.a"
+  "libtodo_lib.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/todo_lib.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ todo/build/CMakeFiles/todo_lib.dir/todo.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/todo_lib.dir/todo.cpp.o: \
+ /home/eric/github/cpp-101/todo/todo.cpp /usr/include/stdc-predef.h
